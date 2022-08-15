b06e7150822a33117317334f698ad6b89dcf885e
Yu-Ren Zhang <e8035669@gmail.com>
2022-08-15 16:21:27 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let hello_world = warp::path::end().map(|| {
+        format!(
+            "Hello World!\nCurrent version: {}\n",
+            env!("CARGO_PKG_VERSION")
+        )
+    });
+
+    let hi = warp::path("hi").map(|| "Hi!\n");
+
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!\n", name));
+
+    let version = warp::path("version").map(|| env!("CARGO_PKG_VERSION"));
+
+    let routes = warp::get().and(hello_world.or(hi).or(hello).or(version));
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
